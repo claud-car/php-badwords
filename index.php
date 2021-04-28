78bee3bbe83e95a87aca5067bf59bd60b960bb7c
claud-car <claudiocarchietti@yahoo.it>
2021-04-28 15:51:15 +0200

    prove php

--- index.php
+++ index.php
@@ -1,12 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <div>pinco pallino</div>
-</body>
-</html>
+    <?php
+        //$testo = 'Claudio Carchietti';
+        //var_dump($_GET['nome']);
+        $testo = $_GET['nome'] . ' ' . $_GET['cognome'];
+    ?>
+
+    <div>
+        <?php //var_dump($testo) ?>
+        Ciao <?php echo $testo ?>
+    </div>
