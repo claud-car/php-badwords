04f9160864c689d3b275dcc01021f2f8ccefa80e
claud-car <claudiocarchietti@yahoo.it>
2021-04-28 16:32:13 +0200

    censura di una parole usando $_GET

--- index.php
+++ index.php
@@ -1,10 +1,24 @@
+<!-- Creare una variabile con un paragrafo di testo.
+ Visualizzare a schermo il paragrafo con la relative
+  lunghezza e sostituire la badword passata in GET con tre * -->
+    
+    
     <?php
         //$testo = 'Claudio Carchietti';
         //var_dump($_GET['nome']);
-        $testo = $_GET['nome'] . ' ' . $_GET['cognome'];
+        // $testo = $_GET['nome'] . ' ' . $_GET['cognome'];
+        
+        $esercizio = 'Lorem ipsum dolor sit amet consectetur Lorem adipisicing elit. Nostrum eaque fuga in Lorem dolorum 
+        suscipit veniam voluptatem! Quam ea sunt, Lorem iusto ipsum vitae accusantium eos';
+        $badWord = $_GET['censura'];
     ?>
 
-    <div>
+
+    <div>  
         <?php //var_dump($testo) ?>
-        Ciao <?php echo $testo ?>
+        <!-- Ciao <?php echo $testo; ?> -->
+        <p><?php echo str_replace($badWord, "***",  $esercizio) ?>.</p>
+        <br>
+        <p>La lunghezza della frase è di <?php echo strlen($esercizio) ?> lettere.</p>
+        
     </div>
